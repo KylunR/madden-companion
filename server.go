9ccaf2b79d7131e5c10178fbc5430f0ca3d1f2d1
Kylun . <kylun.robbins@gmail.com>
2023-10-01 21:19:26 -0700

    Added port env

--- server.go
+++ server.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi! %s", r.URL.Path[1:])
+func handler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hi! %s", req.URL.Path[1:])
 }
 
 func main() {
+	port := os.Getenv("PORT")
+
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	// log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
